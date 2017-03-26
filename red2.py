8c81be6c567ed181c7c29f22568dd4582f615ce0
Demnod <don@Playground.local>
2017-03-26 11:48:22 -0700

    No more top 5
    Changed from just top 5 to Find types of articles and label them

--- red2.py
+++ red2.py
@@ -1,15 +1,85 @@
-import praw
+import feedparser
+import json
+import re
 
-user_agent = ("Demnod Top10 0.1")
 
-r = praw.Reddit("leagueoflegends")
+lol = feedparser.parse('https://www.reddit.com/r/leagueoflegends/.rss')
 
-r = praw.Reddit(user_agent = user_agent)
+arts = len(lol.entries)
+#print len(lol.entries)
 
-subreddit = r.get_subreddit("leagueoflegends")
 
-for submission in subreddit.get_hot(limit=5):
-	print "Title: ", submission.title
-	print "Text: ", submission.selftext
-	print "Score: ", submission.score
-	print "=============\n"
+#Print titles of Front page posts
+print('\n')
+for i in range(arts):
+	print lol.entries[i].title
+
+
+#Get # of entries, go through entries one by one to match author to moobeat, return title for now
+for i in range(arts):
+
+	if lol.entries[i].author == '/u/moobeat': #Find Posts by moobeat
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+		print "Moobeat posts:" + '\n'
+		print 'Title: ' + lol.entries[i].title
+		print 'Date: ' + lol.entries[i].date
+		print 'Link: ' + str(lol.entries[i].links[0].href)
+		print i
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+	if "lcs" in lol.entries[i].title.lower(): #Find Posts with LCS in title
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+		print "LCS articles:" + '\n'
+		print 'Title: ' + lol.entries[i].title
+		print 'Date: ' + lol.entries[i].date
+		print 'Link: ' + str(lol.entries[i].links[0].href)
+		print i
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+	if "dyrus" in lol.entries[i].content[0].value.lower(): #Find Posts with Dyrus in Content section
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+		print "Dyrus mentions:" + '\n'
+		print 'Title: ' + lol.entries[i].title
+		print 'Date: ' + lol.entries[i].date
+		print 'Link: ' + str(lol.entries[i].links[0].href)
+		print i
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+	if "faker" in lol.entries[i].title.lower(): #Find Posts with 24 in title
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+		print "Faker articles:" + '\n'
+		print 'Title: ' + lol.entries[i].title
+		print 'Date: ' + lol.entries[i].date
+		print 'Link: ' + str(lol.entries[i].links[0].href)
+		print i
+		print '\n' '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+	print
+
+
+
+
+'''
+for key in league.keys():
+	if str(key) == "entries":
+		print 'IT HAS BEEN FOUND'
+		test = feedparser.parse(key)
+		print test
+'''
+
+
+'''
+for key in lol.keys():
+	print 'Current Key is ' + str(key)
+	print 'Current Value is ' + str(lol[key])
+	print '\n'
+'''
+
+
+
+'''
+for entry in league['entries']:
+	print "[+] NEW POST TITLE - " + str(entry['title'])
+	print "\n"
+'''
+'''
+for entry in league['entries']:
+	print str(entry)
+	print '\n'
+'''
